a92bffb3db456b9cced5a3bf283a27167a6fa91c
Binal <binal@Binals-MacBook-Pro.local>
2021-08-31 22:11:28 +0100

    Display alert in Text Fieldof minimum 3 characters

--- BJHomework/BJHomework/View/ViewController.swift
+++ BJHomework/BJHomework/View/ViewController.swift
@@ -21,14 +21,34 @@ class ViewController: UIViewController {
         
         guard let name = txtName.text else {
             return
-            
         }
+        if txtName.text?.count ?? 0 > 3 {
+            
+            let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "MovieListViewController") as? MovieListViewController
 
-        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "MovieListViewController") as? MovieListViewController
-
-        vc?.title = "Hello: \(name)"
-    
-        self.navigationController?.pushViewController(vc!, animated: true)
+            vc?.title = "Hello: \(name)"
+        
+            self.navigationController?.pushViewController(vc!, animated: true)
+        }
+        else{
+            let alert = UIAlertController(title: "Alert", message: "Minimum 3 characters", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                switch action.style{
+                    case .default:
+                    print("default")
+                    
+                    case .cancel:
+                    print("cancel")
+                    
+                    case .destructive:
+                    print("destructive")
+                    
+                @unknown default:
+                    fatalError()
+                }
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
 }
